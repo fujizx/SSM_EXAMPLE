a8eb0251c21df7ae454600cd0ab800739474347f
fujizx <fujizx@163.com>
2019-04-12 22:01:49 +0800

    Initial commit

---
+++ SsmFramework/src/main/java/cn/fujizx/SsmFramework/App.java
@@ -0,0 +1,13 @@
+package cn.fujizx.SsmFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
