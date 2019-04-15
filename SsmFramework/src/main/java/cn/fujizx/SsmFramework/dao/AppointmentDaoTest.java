07484f75c46288ce0bcec19d671af7c152a1d5ff
fujizx <fujizx@163.com>
2019-04-15 17:40:31 +0800

    搭建过程

---
+++ SsmFramework/src/main/java/cn/fujizx/SsmFramework/dao/AppointmentDaoTest.java
@@ -0,0 +1,32 @@
+package cn.fujizx.SsmFramework.dao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.fujizx.SsmFramework.BaseTest;
+import cn.fujizx.SsmFramework.domain.Appointment;
+
+
+public class AppointmentDaoTest extends BaseTest {
+
+	@Autowired
+	private AppointmentDao appointmentDao;
+
+	@Test
+	public void testInsertAppointment() throws Exception {
+		long bookId = 1000;
+		long studentId = 12345678910L;
+		int insert = appointmentDao.insertAppointment(bookId, studentId);
+		System.out.println("insert=" + insert);
+	}
+
+	@Test
+	public void testQueryByKeyWithBook() throws Exception {
+		long bookId = 1000;
+		long studentId = 12345678910L;
+		Appointment appointment = appointmentDao.queryByKeyWithBook(bookId, studentId);
+		System.out.println(appointment);
+		System.out.println(appointment.getBook());
+	}
+
+}
